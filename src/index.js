ef68b5c5949892d166a6b4cf97db5ece948eabe8
MaximSelivanov <max_sell@mail.ru>
2021-02-20 14:02:58 +0300

    feat add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;}
+  return Math.min(0, ...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;}
+  return Math.max(0, ...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;}
+  let sum = 0;
+  for (var i = 0; i < array.length; i++) {
+    sum = sum + array[i]
+  }
+  return (sum/array.length)
 }
